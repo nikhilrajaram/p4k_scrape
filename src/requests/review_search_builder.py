bc28c441b0c4c3d0b5ae09b74605734dba604874
nikhilrajaram <nira8877@colorado.edu>
2020-05-11 12:04:40 -0600

    add/encapsulate validation

--- src/requests/review_search_builder.py
+++ src/requests/review_search_builder.py
@@ -1,4 +1,5 @@
 import urllib
+import datetime
 
 
 class ReviewSearchBuilder:
@@ -8,6 +9,7 @@ class ReviewSearchBuilder:
     ROOT_TYPES = "types="
     ROOT_SORT = "sort="
     COMMA_URLENCODED = "%2C"
+    DATESTRING = "%Y-%m-%d"
 
     def __init__(self):
         self.query_string = ReviewSearchBuilder.ROOT_QUERY
@@ -18,6 +20,18 @@ class ReviewSearchBuilder:
         self.query_start_string = None
         self.query_from_string = None
         self.query_to_string = None
+
+    @staticmethod
+    def validate_int(n):
+        if type(n) is not int:
+            raise ValueError("size argument must be of type int")
+
+    @staticmethod
+    def validate_date(date):
+        try:
+            datetime.datetime.strptime(date, ReviewSearchBuilder.DATESTRING)
+        except ValueError:
+            raise ValueError("date format must match 'yyyy-mm-dd")
 
     def add_types_query(self, types):
         if self.query_types_string != ReviewSearchBuilder.ROOT_TYPES:
@@ -41,18 +55,22 @@ class ReviewSearchBuilder:
         return self
 
     def add_size_query(self, size):
+        ReviewSearchBuilder.validate_int(size)
         self.query_size_string = urllib.parse.urlencode({"size": size})
         return self
 
     def add_start_query(self, start):
+        ReviewSearchBuilder.validate_int(start)
         self.query_start_string = urllib.parse.urlencode({"start": start})
         return self
 
     def add_from_query(self, from_date):
+        ReviewSearchBuilder.validate_date(from_date)
         self.query_from_string = urllib.parse.urlencode({"from": from_date})
         return self
 
     def add_to_query(self, to_date):
+        ReviewSearchBuilder.validate_date(to_date)
         self.query_to_string = urllib.parse.urlencode({"to": to_date})
         return self
 
